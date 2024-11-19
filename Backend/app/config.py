b548ee6677084221a76ec1239afe2df9ad9a22b9
Gachuri <rushionchegge@gmail.com>
2024-11-19 17:07:32 +0300

    reverted backend 2

--- Backend/app/config.py
+++
@@ -1,6 +0,0 @@
-# app/config.py
-
-class Config:
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///movers.db'  # Path to your SQLite DB
-    JWT_SECRET_KEY = 'your-secret-key'  # Change this to a secure key
